6cd2760aaac457fda730b53175ef23bc729dd27d
Jascha030 <contact@jaschavanaalst.nl>
2023-02-06 01:07:42 +0100

    ✨ Builder and Config Implementations.

---
+++ src/Builder/Builder.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jascha030\DI\Builder;
+
+use Closure;
+use InvalidArgumentException;
+use Jascha030\DI\ContainerConfigInterface;
+use Psr\Container\ContainerInterface;
+use Jascha030\DI\Config\ContainerConfig;
+use Jascha030\DI\BuilderInterface;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionFunction;
+use function class_implements;
+use function is_subclass_of;
+use function reset;
+use function sprintf;
+
+class Builder implements BuilderInterface
+{
+    /**
+     * @var Closure
+     */
+    private Closure $factory;
+
+    /**
+     * @param callable $callable
+     *
+     * @throws ReflectionException
+     */
+    public function __construct(callable|Closure $callable)
+    {
+        $this->setFactory($callable(...));
+    }
+
+    public function __invoke(ContainerConfig $config): ContainerInterface
+    {
+        return ($this->factory)($config);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function setFactory(Closure $factory): void
+    {
+        $reflection = new ReflectionFunction($factory);
+
+        if ($reflection->getNumberOfParameters() > 1) {
+            throw throw self::invalidArgumentException();
+        }
+
+        $params = $reflection->getParameters();
+        $param  = reset($params);
+
+        if ($param->getType()?->isBuiltin()) {
+            throw self::invalidArgumentException();
+        }
+
+        $type = new ReflectionClass($param->getType()?->getName());
+
+        if (
+            ! $type->isSubclassOf(ContainerConfigInterface::class)
+            && $type->getName() !== ContainerConfigInterface::class
+        ) {
+            throw self::invalidArgumentException();
+        }
+
+        $this->factory = $factory;
+    }
+
+    private static function invalidArgumentException(): InvalidArgumentException
+    {
+        return new InvalidArgumentException(
+            sprintf('Factory callable should take 1 argument of type :%s.', ContainerConfigInterface::class)
+        );
+    }
+}
---
+++ src/Config/ContainerConfig.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jascha030\DI\Config;
+
+use Jascha030\DI\ContainerConfigInterface;
+
+class ContainerConfig implements ContainerConfigInterface
+{
+    private array $definitions = [];
+
+    private array $providers = [];
+
+    private function __construct()
+    {
+    }
+
+    public static function create(): self
+    {
+        return new self();
+    }
+
+    public function withDefinitions(array $definitions): self
+    {
+        $new              = clone $this;
+        $new->definitions = $definitions;
+        return $new;
+    }
+
+    public function getDefinitions(): array
+    {
+        return $this->definitions;
+    }
+
+    /**
+     * @param array $providers Service providers to get definitions from.
+     */
+    public function withProviders(array $providers): self
+    {
+        $new            = clone $this;
+        $new->providers = $providers;
+        return $new;
+    }
+
+    public function getProviders(): array
+    {
+        return $this->providers;
+    }
+}
