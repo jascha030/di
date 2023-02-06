64b6d644def0b856151edddd02263a240c2556de
Jascha030 <contact@jaschavanaalst.nl>
2023-02-06 01:09:57 +0100

    ✨ ContainerProxy

---
+++ src/ContainerProxyTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jascha030\DI;
+
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+trait ContainerProxyTrait
+{
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     *
+     * @return mixed
+     */
+    public function get(string $id): mixed
+    {
+        return $this->getInnerContainer()->get($id);
+    }
+
+    public function has(string $id): bool
+    {
+        return $this->getInnerContainer()->has($id);
+    }
+
+    abstract private function getInnerContainer(): ContainerInterface;
+}
